277ed28ee30695134b45e621c6ac15fcb0f8da89
James <spamonlyjames@gmail.com>
2020-05-24 17:06:12 +0700

    first commit

---
+++ lib/private_example.rb
@@ -0,0 +1,6 @@
+require "private_example/version"
+
+module PrivateExample
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/private_example/version.rb
@@ -0,0 +1,3 @@
+module PrivateExample
+  VERSION = "0.1.0"
+end
